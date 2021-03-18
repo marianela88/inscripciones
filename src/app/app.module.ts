128a92c412281fa5fe31df1a0f06ae5d77e8e404
Marianela1807 <45949889+Marianela1807@users.noreply.github.com>
2021-03-18 09:40:14 -0300

    formulario inscripcion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlumnoComponent } from './alumno/alumno/alumno.component';
 import { TutorComponent } from './tutor/tutor/tutor.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { InscripcionComponent } from './inscripcion/inscripcion/inscripcion.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AlumnoComponent,
-    TutorComponent      
+    TutorComponent,
+    InscripcionComponent      
   ],
   imports: [
     BrowserModule,
